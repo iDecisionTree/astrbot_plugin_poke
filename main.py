505090cffefcc047e7c6e1a99d2566051d7993bd
iDecisionTree <lhdzyc@163.com>
2026-08-25 12:40:24 +0800

    111

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+import datetime
+
 from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.core.message.components import ComponentType
@@ -12,10 +15,29 @@ class MyPlugin(Star):
 
     @filter.platform_adapter_type(filter.PlatformAdapterType.AIOCQHTTP)
     async def poke(self, event: AstrMessageEvent):
+        if event.message_obj.message and event.message_obj.message[0].type is ComponentType.Poke and str(event.message_obj.message[0].id) == event.get_self_id():
 
-        if event.message_obj.message and event.message_obj.message[0].type is ComponentType.Poke and event.message_obj.message[0].id == event.get_self_id():
-            chain = [
-                Component.Plain("喵")
-            ]
-            logger.info("触发戳一戳")
+            today = datetime.date.today()
+            random.seed(f"{today}_{event.get_sender_id()}")
+
+            score = random.randint(0, 100)
+
+            if score == 100:
+                desc = f"你今天的人品为：100！100！100！！！！！！"
+            elif score >= 95:
+                desc = f"你今天的人品为：{score}！差点就到100了呢……"
+            elif score >= 90:
+                desc = f"你今天的人品为：{score}！好评如潮喵！"
+            elif score >= 70:
+                desc = f"你今天的人品为：{score}！今天运气不错呢喵！"
+            elif score >= 40:
+                desc = f"你今天的人品为：{score}，还行啦，还行啦。"
+            elif score >= 20:
+                desc = f"你今天的人品为：{score}！呜……"
+            elif score >= 10:
+                desc = f"你今天的人品为：{score}？！不会吧……"
+            else:
+                desc = f"你今天的人品为：{score}……（是百分制哦）"
+
+            chain = [Component.Plain(desc)]
             yield event.chain_result(chain)
